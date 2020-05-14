71c975bf173ec6faff7cb8ab549610a7cb225f2b
Shravan Kumar <b.j.shravan.kumar@philips.com>
2020-05-14 12:51:36 +0530

    Updated to latest AppInfra

--- ci-build-support/Versions.rb
+++ ci-build-support/Versions.rb
@@ -1,6 +1,6 @@
 if not defined?(CentralizedVersions)
 #    CentralizedVersions = true
-    DependencyCDP2Platform = '~> 2001.1.1586147703'
-    VersionCSWPlatform = '1901.0.1111111111'
+    DependencyCDP2Platform = '~> 2002.0.1588854383'
+    VersionCSWPlatform = '2002.0.1111111111'
     PhilipsUIKitDLSVersion = '~> 3.0.0'
 end
