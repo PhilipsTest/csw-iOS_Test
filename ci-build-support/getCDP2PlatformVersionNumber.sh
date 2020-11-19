2c8ac9c1d897144e9d2a152f78df4d622857e892
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2020-11-20 02:17:52 +0530

    Create getCDP2PlatformVersionNumber.sh

---
+++ ci-build-support/getCDP2PlatformVersionNumber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+VERSIONS_FILE_PATH="ci-build-support/Versions.rb"
+
+if [ ! -f "${VERSIONS_FILE_PATH}" ]
+then
+    echo ""
+    exit 1
+fi
+
+VERSION_REGEX="VersionCSWPlatform[^'|\\"]*['|\\"]([^'|\\"]*)['|\\"]"
+
+cat $VERSIONS_FILE_PATH | egrep -o $VERSION_REGEX | sed -E "s/$VERSION_REGEX/\\1/"
