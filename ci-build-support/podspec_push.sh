75b102b2db6a7c62970693c34617247efb52f714
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2020-11-19 14:25:00 +0530

    Update podspec_push.sh

--- ci-build-support/podspec_push.sh
+++ ci-build-support/podspec_push.sh
@@ -36,11 +36,11 @@ selectPodspecRepo() {
 	if [[ "$BRANCH" == develop* ]]
 	then
 		PODSPEC_REPO_ALIAS="PI-cocoapod-specs-develop"
-		PODSPEC_REPO_URL="ssh://tfsemea1.ta.philips.com:22/tfs/TPC_Region02/Innersource/_git/csw-ios-podspecs"
+		PODSPEC_REPO_URL="https://github.com/PhilipsTest/csw-iOS-podspecs.git"
 	elif [[ "$BRANCH" == release/*  ||  "$BRANCH" == master* ]]
 	then
 		PODSPEC_REPO_ALIAS="EHPMAT-cocoapod-specs"
-		PODSPEC_REPO_URL="ssh://tfsemea1.ta.philips.com:22/tfs/TPC_Region02/Innersource/_git/csw-ios-podspecs"
+		PODSPEC_REPO_URL="https://github.com/PhilipsTest/csw-iOS-podspecs.git"
 	else
 		echo "[fatal] please check your selected branch! Must be one of the following: develop, release or master!"
 		exit 1
