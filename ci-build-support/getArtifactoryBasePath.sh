4c114ed02a6dc0b97372d615775cf3622e2d464f
Abhilash Issac <66210365+abhilashissac@users.noreply.github.com>
2020-11-20 02:15:44 +0530

    Create getArtifactoryBasePath.sh

---
+++ ci-build-support/getArtifactoryBasePath.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -l
+
+ARTIFACTORY_URL="https://artifactory-ehv.ta.philips.com/artifactory"
+ARTIFACTORY_REPO="iet-mobile-ios-release-local"
+
+echo "$ARTIFACTORY_URL/$ARTIFACTORY_REPO/com/philips/platform"
